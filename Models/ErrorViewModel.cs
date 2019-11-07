69d6772efcf72800015247840d84421606ae2c20
kunaalkumar <kunaal@ashiana.com>
2019-11-06 22:39:23 -0700

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Albmer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
